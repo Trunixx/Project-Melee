6bdcf0ff8661f9c19109597bcee263c3d933416c
Trunixx <daniele.galluccio40@gmail.com>
2026-06-12 20:20:05 +0200

    Fixed a call to the previously removed method

--- scripts/states/walking.gd
+++ scripts/states/walking.gd
@@ -13,7 +13,8 @@ func update(_delta: float) -> void:
 	pass
 	
 func physics_update(delta: float) -> void:
-	actor.do_move_by_mode("ground", delta)
+	actor.apply_ground_move(delta)
+	actor.do_move(delta, actor.stats.gravity)
 
 	if not actor.is_on_floor():
 		finished.emit(FALLING)
